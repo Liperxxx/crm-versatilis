0d91710e5892d1ba8ad5aaa6cbdb1cfeb736da03
Liperxxx <felipesp765@gmail.com>
2026-04-05 12:09:39 -0300

    Updated API_BASE_URL in app.js

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -2,7 +2,7 @@
 // APP.JS - Inicialização da aplicação
 // 
 
-const API_BASE_URL = 'http://localhost:8081/api';
+const API_BASE_URL = 'https://crm-versatilis.up.railway.app/api';
 
 class App {
     constructor() {
